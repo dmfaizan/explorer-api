63dadec44fdf245ad8902ef37aadce5814a4941e
dmfaizan <daffaiscoding@gmail.com>
2025-05-26 22:06:45 +0700

    feat: added hardcoded data and basic logic for endpoints instructed in task

---
+++ enums.js
@@ -0,0 +1,12 @@
+export const ItemType = {
+  Folder: "Folder",
+  Text: "txt",
+  PDF: "pdf",
+  JPEG: "jpeg",
+  PNG: "png",
+  XLSX: "xlsx",
+  DOCX: "docx",
+  MARKDOWN: "md",
+  JSON: "json",
+  JS: "js",
+}
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express')
+
+const { ItemType } = require('./enums.js')
+const { items } = require('./items.js')
+
 const app = express()
 const router = express.Router()
 const port = 3000
@@ -7,20 +11,52 @@ const port = 3000
 app.use(express.json())
 app.use('/api', router)
 
+
 // ----- Routes -----
 router.get('/', (req, res) => {
   res.send('Hello world!')
 })
+
 router.get('/folder', (req, res) => {
+  const path = req.query.path;
 
-  let path = req.query.path;
+  if (path != undefined) {
+    const folder = items.find(item => item.name == path);
+    const contents = items.filter(item => folder.childIds.includes(item.id))
 
-  res.send(path)
+    res.send(contents)
+  } else {
+    res.send(items)
+  }
+
 })
+
 router.post('/folder', (req, res) => {
   const { path, name } = req.body;
 
-  res.send(path, name);
+  const newFolder = {
+    "id": items.length,
+    "name": name,
+    "type": ItemType.Folder,
+    "size": null,
+    "path": path,
+    "created": Date.now(),
+    "childIds": []
+  };
+
+  items.push(newFolder);
+
+  res.send(items);
+})
+
+router.delete('/item', (req, res) => {
+  const { path } = req.body;
+
+  const item = items.find(item => item.path == path);
+  const index = items.indexOf(item);
+  items.splice(index, 1);
+
+  res.send(items)
 })
 
 app.listen(port, () => {

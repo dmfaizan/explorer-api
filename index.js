13df314516a22c0975a090b5bbc037b97fb5519c
dmfaizan <daffaiscoding@gmail.com>
2025-05-26 18:25:19 +0700

    feat: added basic working routes

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const app = express()
+const router = express.Router()
 const port = 3000
 
-app.get('/', (req, res) => {
+// ----- App Configurations -----
+app.use(express.json())
+app.use('/api', router)
+
+// ----- Routes -----
+router.get('/', (req, res) => {
   res.send('Hello world!')
+})
+router.get('/folder', (req, res) => {
+
+  let path = req.query.path;
+
+  res.send(path)
+})
+router.post('/folder', (req, res) => {
+  const { path, name } = req.body;
+
+  res.send(path, name);
 })
 
 app.listen(port, () => {
